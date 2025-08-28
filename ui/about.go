2fdc749b369b6a18c32cf66687bc5624a82abe43
marcs100 <marcs100@gmail.com>
2025-08-28 22:19:27 +0100

    About modified

--- ui/about.go
+++ ui/about.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
@@ -15,10 +14,6 @@ import (
 var custAboutDlg *dialog.CustomDialog
 
 func NewAbout(about main_app.About, parentWindow fyne.Window) {
-
-	var UI_Colours = GetAppColours(GetThemeVariant())
-	bg := canvas.NewRectangle(UI_Colours.MainBgColour)
-	accent := canvas.NewRectangle(UI_Colours.AccentColour)
 	aboutGrid := container.NewGridWithColumns(1,
 		container.NewHBox(
 			widget.NewLabel("Minote: "),
@@ -33,10 +28,7 @@ func NewAbout(about main_app.About, parentWindow fyne.Window) {
 			widget.NewLabel("Maintainer: "),
 			widget.NewRichTextFromMarkdown(fmt.Sprintf("**%s**", about.Maintainer))))
 
-	gridPadded := container.NewPadded(bg, aboutGrid)
-
-	aboutStack := container.NewStack(accent, gridPadded)
-	custAboutDlg = dialog.NewCustom("About", "Ok", aboutStack, parentWindow)
+	custAboutDlg = dialog.NewCustom("About", "Ok", aboutGrid, parentWindow)
 }
 
 func ShowAbout() {
